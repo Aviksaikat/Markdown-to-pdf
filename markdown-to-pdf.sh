26f2cabd1cb948fc64d5a683c7e466bafddd94d4
SAIKAT KARMAKAR <saikickkarma@gmail.com>
2023-10-17 20:35:21 +0100

    Auto-commit changes

--- markdown-to-pdf.sh
+++ markdown-to-pdf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Usage 
 if [ "$#" -ne 2 ]; 
 then 
 	echo "usage: $0 <input.md> <output.md>"
@@ -10,12 +11,13 @@ fi
 # If the template not present cp it to the location 
 if [ ! -e /usr/share/pandoc/data/templates/eisvogel.latex ];
 then 
-	sudo cp ./templates/eisvogel.latex /usr/share/pandoc/data/templates/eisvogel.latex 
+	sudo cp "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/templates/eisvogel.latex /usr/share/pandoc/data/templates/eisvogel.latex 
 fi
 
-pandoc $1 -o $2 --from markdown --template eisvogel --listings
+# Doing the magic
+pandoc $1 -o $2 --from markdown --template eisvogel --listings 2>/dev/null
 
-
+# If the magic is successcful open the pdf
 if [ $? -eq 0 ];
 then 
 	evince $2 || okular $2 
